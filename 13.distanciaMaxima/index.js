05cd028b9c65e32f336bc1a2c62f1e72614d14aa
Lucas Aguiar <lucas.aguiarv@gmail.com>
2023-09-06 16:03:53 -0300

    13 pronto

--- 13.distanciaMaxima/index.js
+++ 13.distanciaMaxima/index.js
@@ -1,15 +1,25 @@
-const linhas = input.trim().split("\n")
-
-let distancia = 0
-const coordenadas = []
-for(let i = 1; i < linhas.length; i++){
-    const coord = linhas[i].split(" ");
-    coordenadas.push({
-        x : parseFloat(coord[0], 10),
-        y : parseFloat(coord[1], 10)
-    })
-}
-
-distancia = Math.sqrt(((coordenadas[0].x - coordenadas[1].x)* 2) + ((coordenadas[0].y - coordenadas[1].y) * 2));
-console.log(distancia)
-
+function processData(input) {
+    const linhas = input.trim().split("\n");
+    const n = parseFloat(linhas[0], 10);
+    const coordenadas = [];
+    
+    for(let i = 1; i < linhas.length; i++){
+        const coord = linhas[i].split(" ");
+        coordenadas.push({
+            x : parseFloat(coord[0], 10),
+            y : parseFloat(coord[1], 10)
+        })
+    }
+    
+    let maiorDistancia = 0
+    for(let c = 0; c < n; c++){
+        for (let d = 1; d < n; d++){
+            let distancia = Math.sqrt(Math.pow(coordenadas[d].x - coordenadas[c].x, 2) + Math.pow(coordenadas[d].y - coordenadas[c].y, 2))
+            if(maiorDistancia < distancia){
+                maiorDistancia = distancia
+            }
+        }
+    }
+    
+    console.log(maiorDistancia)
+} 
