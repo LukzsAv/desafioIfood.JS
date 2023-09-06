e178dd1004ce48e48aa1f5742af3092ec9399063
Lucas Aguiar <lucas.aguiarv@gmail.com>
2023-09-06 13:50:12 -0300

    12 pronto

--- 12.capsLockOn/index.js
+++ 12.capsLockOn/index.js
@@ -1,7 +1,7 @@
 function processData(input) {
-    let texto = input.trim()
+    let texto = input
     
-        if(texto[0] == texto[0].toLowerCase() && texto.slice(1) == texto.slice(1).toUpperCase()){
+        if(texto.slice(0, 1) == texto.slice(0, 1).toLowerCase() && texto.slice(1) == texto.slice(1).toUpperCase()){
         texto = texto.slice(0, 1).toUpperCase() + texto.slice(1).toLowerCase()
         }else if(texto === texto.toUpperCase()){
         texto = texto.toLowerCase()
